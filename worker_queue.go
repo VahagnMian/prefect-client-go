cb1ba639b744a4c019af01d12c8547ab008844b9
Vahagn Martirosyan <vahagn@Vahagns-MacBook-Pro.local>
2023-02-12 12:52:29 +0400

    Modify CreateWorkQueue method to return WorkQueue value

--- worker_queue.go
+++ worker_queue.go
@@ -8,23 +8,31 @@ import (
 	"net/http"
 )
 
-func (wq WorkQueue) CreateWorkQueue(queue WorkQueue) error {
+func (wq WorkQueue) CreateWorkQueue(queue WorkQueue) (*WorkQueue, error) {
 	data, err := json.Marshal(queue)
 	if err != nil {
-		return fmt.Errorf("marshaling error: %v", err)
+		return nil, fmt.Errorf("marshaling error: %v", err)
 	}
 
 	resp, err := http.Post("http://localhost:4200/api/work_queues/", "application/json", bytes.NewBuffer(data))
 	if err != nil {
-		return fmt.Errorf("request error: %v", err)
+		return nil, fmt.Errorf("request error: %v", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
 		body, _ := ioutil.ReadAll(resp.Body)
 		fmt.Printf("response body: %s\n", string(body))
-		return fmt.Errorf("bad status: %s\n%s", resp.Status, body)
+		return nil, fmt.Errorf("bad status: %s\n%s", resp.Status, body)
 	}
 
-	return nil
+	body, err := ioutil.ReadAll(resp.Body)
+
+	work_queue := WorkQueue{}
+	err = json.Unmarshal(body, &work_queue)
+	if err != nil {
+		return nil, err
+	}
+
+	return &work_queue, nil
 }
